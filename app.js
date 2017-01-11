7f5ed4addb962a62ecb7abcc8b166e56c87d0eee
Rafaela <rafaela.vm@gmail.com>
2017-01-11 20:12:42 -0200

    3. Servindo arquivos estáticos

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var express = require('express');
 
 var app = express();
 
+var path = require('path');
+
 //trata as requisições do app
 //request: encapsula os dados que estão sendo recebidos
 //response: encapsula o que eu quero servir como resposta pra essa requisição
 
 app.get('/', function(request, response){
 
-    response.send('Olá, mundo!');
+    response.sendFile(__dirname + '/public/pages/index.html');
 });
 
 app.listen(5050, function(){
